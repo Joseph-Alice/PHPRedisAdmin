71fdccc247f24de405301cdc334060f564ff471a
pengxb <290148030@qq.com>
2017-10-26 14:25:58 +0800

    Update config.sample.inc.php

--- includes/config.sample.inc.php
+++ includes/config.sample.inc.php
@@ -1,71 +1,105 @@
 <?php
-//将此文件复制到config.inc.php并对该文件进行更改，以定制您的配置。
-$config = array(
-  'servers' => array(
-    array(
-      'name'   => 'env-test', // 可选参数
-      'host'   => 'IP',
-      'port'   => 6379,
-      'filter' => '*',
-      'scheme' => 'tcp', // 可选的。连接方案:tcp连接的tcp连接，unix通过unix域套接字连接
-      'path'   => '', // 可选的。通往unix域套接字的路径。只使用"scheme"= >"unix"。例子:' /var/运行/复述/redis.sock '
-      // 可选参数。redis身份验证，【注：无auth的需注释】
-      // 'auth' => '' // 警告:密码以明文发送给Redis服务器。
-    ),
-    array(
-      'name'   => 'env-pre', // 可选参数
-      'host'   => 'IP',
-      'port'   => 6379,
-      'filter' => '*',
-      'scheme' => 'tcp', // 可选的。连接方案。tcp连接的tcp连接，unix通过unix域套接字连接
-      'path'   => '', // 可选的。通往unix域套接字的路径。只使用"scheme"= >"unix"。例子:' /var/运行/复述/redis.sock '
-      // 可选参数。redis身份验证，【注：无auth的需注释】
-      // 'auth' => '' // 警告:密码以明文发送给Redis服务器。
-    ),
-     array(
-      'name'   => 'env-pro', // 可选参数
-      'host'   => 'IP',
-      'port'   => 6379,
-      'filter' => '*',
-      'scheme' => 'tcp', // 可选的。连接方案。tcp连接的tcp连接，unix通过unix域套接字连接
-      'path'   => '', // 可选的。通往unix域套接字的路径。只使用"scheme"= >"unix"。例子:' /var/运行/复述/redis.sock '
-      // 可选参数。redis身份验证，【注：无auth的需注释】
-      'auth' => '' // 警告:密码以明文发送给Redis服务器。
-    ),
-  ),
- 
-  'seperator' => ':',
- 
-  // 取消注释以显示更少的信息，减少redis请求。推荐给一个非常繁忙的Redis服务器。
-  //'faster' => true,
- 
-  // 取消注释以启用HTTP身份验证
-  /*'login' => array(
-    // Username => Password
-    // 可以使用多个组合
-    'admin' => array(
-      'password' => 'adminpassword',
-    ),
-    'guest' => array(
-      'password' => '',
-      'servers'  => array(1) // 可选的服务器列表，该用户可以访问。
-    )
-  ),*/
- 
-  /*'serialization' => array(
-    'foo*' => array( // Match like KEYS
-      // 在保存到redis时调用函数
-      'save' => function($data) { return json_encode(json_decode($data)); },
-      // 从redis加载时调用的函数
-      'load' => function($data) { return json_encode(json_decode($data), JSON_PRETTY_PRINT); },
-    ),
-  ),*/
- 
-  // 您可以忽略下面的设置
-  'maxkeylen'           => 100,
-  'count_elements_page' => 100,
-  // 使用旧的键命令而不是扫描来获取所有的键
-  'keys' => false,
-  // 使用每个扫描命令获取多少个条目
-  'scansize' => 1000
+//Copy this file to config.inc.php and make changes to that file to customize your configuration.
+
+$config = array(
+  'servers' => array(
+    array(
+      'name'   => 'env-test', // 测试线
+      'host'   => 'IP',
+      'port'   => 6379,
+      'filter' => '*',
+      'scheme' => 'tcp', // Optional. Connection scheme. 'tcp' - for TCP connection, 'unix' - for connection by unix domain socket
+      'path'   => '' // Optional. Path to unix domain socket. Uses only if 'scheme' => 'unix'. Example: '/var/run/redis/redis.sock'
+
+      // Optional Redis authentication.
+      //'auth' => 'redispasswordhere' // Warning: The password is sent in plain-text to the Redis server.
+    ),
+    array(
+      'name'   => 'env-pre', // 预上线
+      'host'   => 'IP',
+      'port'   => 6379,
+      'filter' => '*',
+      'scheme' => 'tcp', // Optional. Connection scheme. 'tcp' - for TCP connection, 'unix' - for connection by unix domain socket
+      'path'   => '' // Optional. Path to unix domain socket. Uses only if 'scheme' => 'unix'. Example: '/var/run/redis/redis.sock'
+
+      // Optional Redis authentication.
+      //'auth' => 'redispasswordhere' // Warning: The password is sent in plain-text to the Redis server.
+    ),
+
+     array(
+      'name'   => 'env-pro', // 正式线
+      'host'   => 'IP',
+      'port'   => 6379,
+      'filter' => '*',
+      'scheme' => 'tcp', // Optional. Connection scheme. 'tcp' - for TCP connection, 'unix' - for connection by unix domain socket
+      'path'   => '', // Optional. Path to unix domain socket. Uses only if 'scheme' => 'unix'. Example: '/var/run/redis/redis.sock'
+
+      // Optional Redis authentication.【注：没有密码的需注释】
+      'auth' => 'redispasswordhere' // Warning: The password is sent in plain-text to the Redis server.
+    ),
+
+    /*array(
+      'host' => 'localhost',
+      'port' => 6380
+    ),*/
+
+    /*array(
+      'name'      => 'local db 2',
+      'host'      => 'localhost',
+      'port'      => 6379,
+      'db'        => 1,             // Optional database number, see http://redis.io/commands/select
+      'databases' => 1,             // Optional number of databases (prevents use of CONFIG command).
+      'filter'    => 'something:*', // Show only parts of database for speed or security reasons.
+      'seperator' => '/',           // Use a different seperator on this database (default uses config default).
+      'flush'     => false,         // Set to true to enable the flushdb button for this instance.
+      'charset'   => 'cp1251',      // Keys and values are stored in redis using this encoding (default utf-8).
+      'keys'      => false,         // Use the old KEYS command instead of SCAN to fetch all keys for this server (default uses config default).
+      'scansize'  => 1000           // How many entries to fetch using each SCAN command for this server (default uses config default).
+    ),*/
+  ),
+
+
+  'seperator' => ':',
+
+
+  // Uncomment to show less information and make phpRedisAdmin fire less commands to the Redis server. Recommended for a really busy Redis server.
+  //'faster' => true,
+
+
+  // Uncomment to enable HTTP authentication
+  /*'login' => array(
+    // Username => Password
+    // Multiple combinations can be used
+    'admin' => array(
+      'password' => 'adminpassword',
+    ),
+    'guest' => array(
+      'password' => '',
+      'servers'  => array(1) // Optional list of servers this user can access.
+    )
+  ),*/
+
+
+  /*'serialization' => array(
+    'foo*' => array( // Match like KEYS
+      // Function called when saving to redis.
+      'save' => function($data) { return json_encode(json_decode($data)); },
+      // Function called when loading from redis.
+      'load' => function($data) { return json_encode(json_decode($data), JSON_PRETTY_PRINT); },
+    ),
+  ),*/
+
+
+  // You can ignore settings below this point.
+
+  'maxkeylen'           => 100,
+  'count_elements_page' => 100,
+
+  // Use the old KEYS command instead of SCAN to fetch all keys.
+  'keys' => false,
+
+  // How many entries to fetch using each SCAN command.
+  'scansize' => 1000
 );
+
+?>
